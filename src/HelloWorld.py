fe0452153cb04abf5c9cbaf163d419b917a77442
Tarik <ta355@njit.edu>
2021-02-15 21:19:10 -0500

    Fix: Fixed up HelloWorld.py spacing.

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -2,6 +2,8 @@ def hello(name):
     print("Welcome to python! What's your name?")
     input(name)
     if(name.length<2):
-        print("I asked for your name not your first initial! Give me a real name please.")
+        print("I asked for your name not your first initial! Give me a real name please. ")
     else:
         print("Hey there "+ name+ "You're welcome to code as much as you want!")
+
+
